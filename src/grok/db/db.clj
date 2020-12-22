ba7ab8bcd9f4ddd99b9719d20e3ad12a7e95a390
orlando-mendoza <orlandomr27@gmail.com>
2020-12-22 07:59:20 -0600

    added tests for db connection

---
+++ src/grok/db/db.clj
@@ -0,0 +1,57 @@
+q(ns grok.db.core
+  (:require
+   [datomic.client.api :as d]
+   [grok.db.schema :refer [schema]]))
+
+;; the config
+(def cfg {:server-type :peer-server
+          :access-key "myaccesskey"
+          :secret "mysecret"
+          :endpoint "localhost:8998"
+          :validate-hostnames false})
+;; the client
+(def client (d/client cfg))
+
+;; the connection
+(def conn (d/connect client {:db-name "grok"}))
+
+;; ------------------------------------------------------------
+;; Create schema and transact into the database
+(def tx (d/transact conn {:tx-data schema}))
+
+
+
+(comment
+  ;; First create a database using the REPL from the Peer section
+  ;; cd /Users/omendozar/datomic/datomic-pro-1.0.6222
+  ;; $ bin/repl
+  ;; user=> (require '[datomic.api :as d])
+  ;; nil
+  ;; user=> (def db-uri "datomic:dev://localhost:4334/grok")
+  ;; #'user/db-uri
+  ;; user=> (d/create-database db-uri)
+  ;; true
+  ;; ------------------------------------------------------------
+  ;; Then start a Peer server
+
+  ;; $ bin/run -m datomic.peer-server -h localhost -p 8998 -a myaccesskey,mysecret -d grok,datomic:dev://localhost:4334/grok
+  ;; Serving datomic:dev://localhost:4334/grok as grok
+
+  ;; Transact to test the connection
+
+
+  (keys tx)
+  ;; => (:db-before :db-after :tx-data :tempids)
+
+  (def db-before (:db-before tx))
+  (def db-after (:db-after tx))
+
+  ;; ------------------------------------------------------------
+  ;; Query
+
+  (d/q '[:find ?doc
+         :where
+         [_ :db/doc ?doc]]
+       db-after)
+
+  ,)
---
+++ test/grok/db/with_db.clj
@@ -0,0 +1,21 @@
+(ns grok.db.with-db
+  (:require
+    [grok.db.schema :refer [schema]]
+    [datomic.api :as d]))
+
+(def ^:dynamic *conn* nil)
+
+(defn create-conn [db-uri]
+  (d/create-database db-uri)
+  (let [conn (d/connect db-uri)]
+    conn))
+
+(defn fresh-db []
+  (let [db-uri (str "datomic:mem://" (gensym))
+        conn (create-conn db-uri)]
+    (d/transact conn schema)
+    conn ))
+
+(defn with-db [f]
+  (binding [*conn* (fresh-db)]
+    (f)))
